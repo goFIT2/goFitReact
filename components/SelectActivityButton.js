c7b74220812c553c3d335ef6671e72b1592cc3b0
Christian Valadez <christian@DN0a226506.SUNet>
2018-01-22 18:20:30 -0800

    Added Select Activity Button

--- components/SelectActivityButton.js
+++ components/SelectActivityButton.js
@@ -1,10 +1,67 @@
 import React from 'react'
+import { 
+    TouchableHighlight, 
 
+    } from 'react-native'
 
+const imageLinks = {
+    Swimming: '../assets/images/swim.png', 
+    Running : '../assets/images/run.png',
+    Salad: '../assets/images/salad.png',
+    Walking: '../assets/images/footsteps.png',
+    Biking: '../assets/images/bike.png',
+    Weights: '../assets/images/weights.png',
+    Jumprope: '../assets/images/jumprope.png',
+    Water: '../assets/images/water.png'
+}
+    
 class SelectActivityButton extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    getURL(activityType) {
+        let url = ''
+        switch (activityType){
+            case 'Swimming':
+                url = '../assets/images/swim.png'
+            case 'Running':
+                url = '../assets/images/run.png'
+            case 'Salad':
+                url = '../assets/images/salad.png' 
+            case 'Walking':
+                url = '../assets/images/footsteps.png' 
+            case 'Biking':
+                url = '../assets/images/bike.png'
+            case 'Weights':
+                url = '../assets/images/weights.png'
+            case 'Jumprope':
+                url = '../assets/images/jumprope.png'
+            case 'Water':
+                url = '../assets/images/water.png'
+            default:
+                console.log("Didn't pass in the proper activity type")
+        }
+        console.log("URL")
+        console.log(url)
+        return require(url) 
+    }
+
     render(){
+        let imageSource = getURL(this.props.activityType)
         return(
-            <Text> nothing to see here </Text>
+            <TouchableHighlight
+            style={styles.activityStyle}
+            onPress={this.activityPress}
+            >   
+            <View> 
+                <Image source={imageSource}
+                    style={styles.imageStyle}
+                    resizeMode='contain'
+                />
+                <Text>Swimming</Text>
+            </View> 
+        </TouchableHighlight>
         )
     }
 }
--- screens/CreateChallengeScreen.js
+++ screens/CreateChallengeScreen.js
@@ -1,65 +1,153 @@
 import React from 'react'
-import { StyleSheet, 
-    View, 
-    Text, 
-    TextInput, 
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
     SegmentedControlIOS,
-    FlatList
+    FlatList,
+    Image,
+    ScrollView,
+    TouchableHighlight
 } from 'react-native'
 import SimpleStepper from 'react-native-simple-stepper'
+import SelectActivityButton from '../components/SelectActivityButton'
 
-class CreateChallengeScreen extends React.Component { 
+class CreateChallengeScreen extends React.Component {
+    activityPress = () => { 
 
-    renderActivity = ({item}) => {
-        return (
-            <Text>
-                {item.key}
-            </Text>
-        )        
     }
 
-    render(){
-        return(
-            <View>
-                <Text>Select Activity</Text> 
-
-                <FlatList 
-                    data={[{key: 'a'}, {key: 'b'}, {key: 'c'}]}
-                    renderItem={({item}) => <Text>{item.key}</Text>}
-                /> 
-
-                <Text>Set Goal</Text> 
-                <View style={{flexDirection: 'row'}}>
-                    <View style={{flexDirection: 'column', flex:1, margin:10}}> 
-                        <TextInput
-                            style={{borderColor: 'gray', borderWidth: 1}}
-                            keyboardType='numeric'
-                        />
-                        <SegmentedControlIOS 
-                            values={['hr', 'min', 'km', 'mi']}
-                            selectedIndex={0}
-                            onChange={(event) => {
-                                this.setState({selectedIndex: event.nativeEvent.selectedSegmentIndex});
-                            }}
-                        />
-                    </View> 
-                            
-                    <View style={{flexDirection: 'column', flex:1, margin:10}}> 
-                        <Text> 3x per Week </Text> 
-                        <SimpleStepper 
-                        /> 
+    render() {
+        return (
+            <ScrollView>
+                <View>
+                    <Text>Select Activity</Text>
+                    <View
+                        style={styles.selectActivityStyle}
+                    >
+                        <View
+                            style={styles.activityRowStyle}
+                        >
+                            <SelectActivityButton 
+                                activityType='Swimming'
+                            />
+                            <View
+                                style={styles.activityStyle}  >
+                                <Image source={require('../assets/images/run.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Running</Text>
+                            </View>
+                            <View
+                                style={styles.activityStyle}
+                            >
+                                <Image source={require('../assets/images/salad.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Salad</Text>
+                            </View>
+                            <View
+                                style={styles.activityStyle}
+                            >
+                                <Image source={require('../assets/images/footsteps.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Walking</Text>
+                            </View>
+                        </View>
+                        <View
+                            style={styles.activityRowStyle}
+                        >
+                            <View
+                                style={styles.activityStyle}
+                            >
+                                <Image source={require('../assets/images/bike.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Biking</Text>
+                            </View>
+                            <View
+                                style={styles.activityStyle}  >
+                                <Image source={require('../assets/images/weights.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Weights</Text>
+                            </View>
+                            <View
+                                style={styles.activityStyle}
+                            >
+                                <Image source={require('../assets/images/jumprope.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Jumprope</Text>
+                            </View>
+                            <View
+                                style={styles.activityStyle}
+                            >
+                                <Image source={require('../assets/images/water.png')}
+                                    style={styles.imageStyle}
+                                    resizeMode='contain'
+                                />
+                                <Text>Water</Text>
+                            </View>
+                        </View>
                     </View>
                 </View> 
 
-                <Text>Select Participants</Text> 
-            </View> 
+                <View> 
+                    <Text>Set Goal</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ flexDirection: 'column', flex: 1, margin: 10 }}>
+                            <TextInput
+                                style={{ borderColor: 'gray', borderWidth: 1 }}
+                                keyboardType='numeric'
+                            />
+                            <SegmentedControlIOS
+                                values={['hr', 'min', 'km', 'mi']}
+                                selectedIndex={0}
+                                onChange={(event) => {
+                                    this.setState({ selectedIndex: event.nativeEvent.selectedSegmentIndex });
+                                }}
+                            />
+                        </View>
+
+                        <View style={{ flexDirection: 'column', flex: 1, margin: 10 }}>
+                            <Text> 3x per Week </Text>
+                            <SimpleStepper
+                            />
+                        </View>
+                    </View>
+                </View> 
+
+            </ScrollView>
         )
     }
 }
 const styles = StyleSheet.create({
     flex: {
 
+    },
+    imageStyle: {
+        height: 50, 
+        width: 50,
+        resizeMode: 'contain'
+    },
+    activityStyle: {
+        flex: 1, alignItems: 'center' 
+    },
+    activityRowStyle: { 
+        flex: 1, flexDirection: 'row' 
+    },
+    selectActivityStyle: {
+        flexDirection: 'column'
     }
-}) 
+})
 
 export default CreateChallengeScreen
