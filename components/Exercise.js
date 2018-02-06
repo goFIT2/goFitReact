e06d9f3a14ccc8e0c18b5b406dcbab76116616b9
Christian Valadez <christian@DN0a25b84b.SUNet>
2018-02-05 18:58:14 -0800

    working on SectionList for progress, refactored to use Pure Components, but need to maintain state for progress Row

--- components/Exercise.js
+++ components/Exercise.js
@@ -1,7 +1,35 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { SectionList, TouchableHighlight, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
 
+
+const exerciseTitle = (props) => {
+    return (
+        <TouchableOpacity style={styles.title}>
+            <Text>Bench Press</Text>
+            <Entypo name='chevron-right' style={styles.chevron} /> 
+        </TouchableOpacity>
+    )
+}
+
+const columnHead = () => {
+    return(
+        <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
+            <View style={styles.columnText1}>
+                <Text style={{alignSelf: 'center'}}>#</Text>
+            </View> 
+            <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
+                <Text style={{alignSelf: 'center'}}>Previous</Text>
+            </View> 
+            <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
+                <Text style={{alignSelf: 'center'}}>Lbs.</Text>
+            </View> 
+            <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
+                <Text style={{alignSelf: 'center'}}>Reps</Text>
+            </View> 
+        </View>
+    )
+}
 
 class Exercise extends React.Component { 
     constructor(props){
@@ -9,45 +37,57 @@ class Exercise extends React.Component {
         this.props.name = "Barbell Press"
     }
     
+    addSet = () => {
+        console.log("button pressed")
+    }
+
     render() {
+        const title = ( 
+
+        )
+
+        const columnHead = (
+
+        )
+        
+        const progressRow = (
+            <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
+                <View style={[styles.columnText1, styles.progressRow]}>
+                    <Text style={{alignSelf: 'center'}}>1</Text>
+                </View> 
+                <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
+                    <Text style={{alignSelf: 'center'}}>1</Text>
+                </View> 
+                <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
+                    <Text style={{alignSelf: 'center'}}>1</Text>
+                </View> 
+                <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
+                    <Text style={{alignSelf: 'center'}}>1</Text>
+                </View> 
+            </View>  
+        )
+
+        const addSet = (
+            <TouchableHighlight 
+                onPress={() => this.addSet()}
+                style={styles.button}
+                >
+                <Text style={{textAlign: 'center', textAlignVertical: 'center', color: 'white'}}>ADD SET</Text>
+            </TouchableHighlight> 
+        )
         return (
             <View>
+                <SectionList
+                    renderItem={progressRow}
+                    ListHeaderComponent={title}
+                    renderSectionHeader={columnHead}
+                    renderSectionFooter={addSet}
+                    sections={[{
+                        data: 1, title: "Barbell"
+                    }]}
+                >
 
-                <TouchableOpacity style={styles.title}>
-                    <Text>Bench Press</Text>
-                    <Entypo name='chevron-right' style={styles.chevron} /> 
-                </TouchableOpacity>
-
-                <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
-                    <View style={styles.columnText1}>
-                        <Text style={{alignSelf: 'center'}}>#</Text>
-                    </View> 
-                    <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>Previous</Text>
-                    </View> 
-                    <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>Lbs.</Text>
-                    </View> 
-                    <View style={[styles.columnText1, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>Reps</Text>
-                    </View> 
-                </View>
-
-                <View style={{flexDirection: 'row', paddingLeft: 10, paddingRight: 10}}>
-                    <View style={[styles.columnText1, styles.progressRow]}>
-                        <Text style={{alignSelf: 'center'}}>1</Text>
-                    </View> 
-                    <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>1</Text>
-                    </View> 
-                    <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>1</Text>
-                    </View> 
-                    <View style={[styles.columnText1, styles.progressRow, {borderLeftWidth: 0}]}>
-                        <Text style={{alignSelf: 'center'}}>1</Text>
-                    </View> 
-                </View>  
-
+                </SectionList>
             </View> 
         )
     }
@@ -84,4 +124,10 @@ const styles = StyleSheet.create({
         borderColor: '#D8D8D8',
         borderTopWidth: 0
     },
+    button: {
+        alignItems: 'center', 
+        marginLeft: 10, 
+        marginRight: 10,
+        backgroundColor: '#FB6D00'
+    }
 })
